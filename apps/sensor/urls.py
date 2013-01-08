f23d8e930edca3bd658aeaca3327996b505b0410
Jon Latorre <moebius@etxea.net>
2013-01-08 14:58:20 +0100

    primera version funcional con comunicación con el geekmostatO

--- apps/sensor/urls.py
+++ apps/sensor/urls.py
@@ -3,7 +3,7 @@ from django.views.generic.simple import direct_to_template
 from views import *
 
 urlpatterns = patterns("",
-    url(r"^leer/$", leer , name="leer"),
-    url(r"^set/(\d{1})/$", escribir , name="escribir"),
-    #url(r"^$", direct_to_template, {"template": "geekmostato/index.html"}, name="index"),
+    url(r"^get/$", leer , name="sensor_get"),
+    url(r"^set/(\d{1})/$", escribir , name="sensor_set"),
+    url(r"^$", direct_to_template, {"template": "sensor/index.html"}, name="sensor_index"),
 )
