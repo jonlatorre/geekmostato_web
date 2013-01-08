a82a6e79fbd993f8eed638f9cbb2cdf095f711e1
Jon Latorre <moebius@etxea.net>
2013-01-08 15:42:47 +0100

    Ahora enviamos la temp como 2 digitos

--- apps/sensor/urls.py
+++ apps/sensor/urls.py
@@ -4,6 +4,6 @@ from views import *
 
 urlpatterns = patterns("",
     url(r"^get/$", leer , name="sensor_get"),
-    url(r"^set/(\d{1})/$", escribir , name="sensor_set"),
+    url(r"^set/(\d{2})/$", escribir , name="sensor_set"),
     url(r"^$", direct_to_template, {"template": "sensor/index.html"}, name="sensor_index"),
 )
